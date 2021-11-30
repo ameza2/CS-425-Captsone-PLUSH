08b529e2215019e4c6808eec027618f047887d0f
Abraham Meza <60674138+ameza2@users.noreply.github.com>
2021-11-29 20:00:09 -0800

    Update Tinker_PLUSH.ino
    Today, I implemented the following:
    
    - Volume Sensitivity Dial that displays Volume Level on LCD when changed.
    - Arduino Music Library (Temp Song: Mario Theme)
        - Currently the music library is being stored in a separate Arduino to minimize button delays. Right now, the music feature is programmed to send frequency beeps and uses delays to ensure proper pauses between notes to make a cohesive tune. If the music library was included in the original Arduino, then there would be too much delay, affecting button processing and not updating the LCD properly
    - Music Toggle Feature (kinda)
    
    Issues I’m having:
    
    - If you press the music button, the music is turned on. If you press it again, it may or may not turn off (inconsistent). I’m having trouble thinking about a way to continuously check for a button toggle while also playing musical notes w/ delays because if a user pressed the button during a delay, the button press is ignored :((. Please help.
    - I’m also having trouble figuring out how to implement volume levels into the speaker. I can easily do it with a potentiometer; however, if I use a potentiometer to configure beeper volume, then it won’t have a way to print to the LCD, and vice-versa. So how do I get the same potentiometer to configure volume sensitivity on the speaker, and also display volume sensitivity to the LCD. Please also help.
    Pushing new changes soon.

--- ArduinoCode/Tinker_PLUSH.ino
+++ ArduinoCode/Tinker_PLUSH.ino
@@ -7,9 +7,8 @@
 
 /* Things to Work On:
 
-- Fix LCD Color
-- Add Volume Dial w/ LCD Display (PotentioMeter)
-- Add Speaker tied to Volume Dial and Music Button? (Beeper)
+- Figure out how to untoggle music accurately
+- Figure out how to apply volume changes to both lcd and piezo
 - Add Servos tied to Hug Button?
 
 */
@@ -36,85 +35,112 @@ unsigned const LEDR = 1; // assign LED Red digital pin
 unsigned const LEDB = 2; // assign LED Blue digital pin
 unsigned const LEDG = 3; // assign LED Green digital pin
 
+
+// Music Variable(s) //
+
+unsigned const volumeDial = A5; // assign volumeDial digital pin
+unsigned int newVolume = 0; // variable: used to store new volume setting
+unsigned int oldVolume = 0; // variable: used to store old volume setting
+
 // String Dictionary //
 
 String buttonMessages[] = { // string array: used to store LCD button prompts
-    "Call button",
-    "Hug button",
-    "Emergency button",
-    "Music button"
+  "Call button",
+  "Hug button",
+  "Emergency button",
+  "Music button"
 };
 
-void setup(){
-    
-    // Serial Port Check (Debugging) //
-    
-    Serial.begin(9600); // initialize serial
-    while (!Serial) { // while loop: while serial port is closed, wait for serial port to connect (needed for Native USB)
-        ; // do nothing
-    }
-    
-    // Random Number Generator //
-    
-    randomSeed(analogRead(0));
-    
-    // Input Pins //
-    
-    pinMode(helpButton, INPUT);
-    pinMode(hugButton, INPUT);
-    pinMode(emergencyButton, INPUT); 
-    pinMode(musicButton, INPUT);
-    
-    // Output Pins //
-    
-    pinMode(LEDR, OUTPUT);
-    pinMode(LEDG, OUTPUT);
-    pinMode(LEDB, OUTPUT);
-    
-    // LCD Initialization //
-    
-    lcd.begin(16, 2);  // set up the LCD's number of columns and rows:
+void setup() {
+  
+  // Serial Port Check (Debugging) //
+  
+  Serial.begin(9600); // initialize serial
+  while (!Serial) { // while loop: while serial port is closed, wait for serial port to connect (needed for Native USB)
+    ; // do nothing
+  }
+  
+  // Random Number Generator //
+  
+  randomSeed(analogRead(0));
+  
+  // Input Pins //
+  
+  pinMode(helpButton, INPUT);
+  pinMode(hugButton, INPUT);
+  pinMode(emergencyButton, INPUT); 
+  pinMode(musicButton, INPUT);
+  
+  // Output Pins //
+  
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  
+  // LCD Initialization //
+  
+  lcd.begin(16, 2);  // set up the LCD's number of columns and rows:
 }
 
 void loop(){
-    // Update Button States //
-    
-    buttonStates[0] = digitalRead(helpButton); // help button state
-    buttonStates[1] = digitalRead(hugButton); // hug button state
-    buttonStates[2] = digitalRead(emergencyButton); // emergency button state
-    buttonStates[3] = digitalRead(musicButton); // music button state
+  
+  // Update Button States //
+  
+  buttonStates[0] = digitalRead(helpButton); // help button state
+  buttonStates[1] = digitalRead(hugButton); // hug button state
+  buttonStates[2] = digitalRead(emergencyButton); // emergency button state
+  buttonStates[3] = digitalRead(musicButton); // music button state
+  
+  // Update Volume State //
+  
+  newVolume = analogRead(volumeDial); // new volume level state (potentiometer)
+  newVolume = map(newVolume, 0, 1024, 0, 100); // map volume level boundaries (0 - 1024 -> 0 - 100)
 
-    // Check Button States: Output Toggle Message //
-    
-    for(int i = 0; i < sizeof(buttonStates) / sizeof(int); i++) { // for loop: check to see if PLUSH unit buttons have been pressed. If any buttons were pressed, send signal to PLUSH application    
-        if(buttonStates[i] == HIGH) { // if statement: if button is pressed, send signal to PLUSH application    
-            lcd.clear(); // clear LCD display/data and position cursor in upper-left corner
-            
-            lcd.setCursor(0,0); // set LCD display cursor back to [0,0]: first row
-            
-            lcd.print(buttonMessages[i]); // print button state message
-            
-            lcd.setCursor(0,1); // set LCD display cursor to [0,1]: second row
-            
-            lcd.print("pressed!"); // print toggle messsage
-            
-            if(buttonMessages[i] == "Call button") {
-                RGB_color(255, 255, 255); // White
-                delay((random(10) * 1000));
+  // Check Button States: Output Toggle Message //
+  
+  for(int i = 0; i < sizeof(buttonStates) / sizeof(int); i++) { // for loop: check to see if PLUSH unit buttons have been pressed. If any buttons were pressed, send signal to PLUSH application    
+    if(buttonStates[i] == HIGH) { // if statement: if button is pressed, send signal to PLUSH application    
+      lcd.clear(); // clear LCD display/data and position cursor in upper-left corner
+      
+      lcd.setCursor(0,0); // set LCD display cursor back to [0,0]: first row
+      
+      lcd.print(buttonMessages[i]); // print button state message
+      
+      lcd.setCursor(0,1); // set LCD display cursor to [0,1]: second row
+      
+      lcd.print("pressed!"); // print toggle messsage
+      
+      if(buttonMessages[i] == "Call button") { // if statement: if active button is call button, change RGB LED to alert patient of request status
+        setColor(0, 0, 0); // Clear (reset)
+        
+        setColor(255, 255, 255); // White (in need of assistance)
+        delay((random(10) * 1000));
 
-                RGB_color(0, 0, 255); // Green
-                delay((random(10) * 1000));
+        setColor(0, 255, 0); // Green (assistance is on the way)
+        delay(5000);
 
-                RGB_color(0, 0, 0); // Clear 
-            }
-        }
+        setColor(0, 0, 0); // Clear (reset)
+      }
     }
-    
-    delay(100); // delay 1ms before checking if another buttoon has been pressed.
+  }
+  
+  if (oldVolume != newVolume) { // if statement: if old volume != new volume, update LCD display (and potentially Piezo) 
+    lcd.clear(); // clear LCD display/data and position cursor in upper-left corner
+
+    lcd.setCursor(0,0); // set LCD display cursor back to [0,0]: first row
+    lcd.print("Volume Level:"); // print button toggle message
+
+    lcd.setCursor(0,1); // set LCD display cursor to [0,1]: second row
+    lcd.print(newVolume); // print toggle messsage
+  }
+  
+  oldVolume = newVolume; // store new volume value as old volume value (don't always display volume setting)
+  
+  delay(100); // delay 1ms before checking if another button has been pressed.
 }
 
-void RGB_color(int redVal, int greenVal, int blueVal) {
-    analogWrite(LEDR, redVal);
-    analogWrite(LEDG, greenVal);
-    analogWrite(LEDB, blueVal);
+void setColor(int redVal, int greenVal, int blueVal) { // setColor: calibrate LED color code
+  analogWrite(LEDR, redVal); // calibrate LEDR
+  analogWrite(LEDG, greenVal); // calibrate LEDG
+  analogWrite(LEDB, blueVal); // calibrate LEDB
 }
