3d3e3fe789230431d422e086ead1a7f0f258fd5a
tszemach17 <tzemach@nevada.unr.edu>
2021-12-04 18:58:53 -0800

    Tied up the software details screen to the settings page

--- PLUSHApp/app/src/main/java/com/example/plush/StaffSettingsScreen.java
+++ PLUSHApp/app/src/main/java/com/example/plush/StaffSettingsScreen.java
@@ -2,7 +2,10 @@ package com.example.plush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StaffSettingsScreen extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class StaffSettingsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_settings_screen);
+
+
+        Button softwareDetailsButton;
+
+        softwareDetailsButton = (Button) findViewById(R.id.SoftwareDetailsButtonID);
+        softwareDetailsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(StaffSettingsScreen.this, staff_SoftwareDetailsScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- PLUSHApp/app/src/main/java/com/example/plush/staff_SoftwareDetailsScreen.java
+++ PLUSHApp/app/src/main/java/com/example/plush/staff_SoftwareDetailsScreen.java
@@ -2,7 +2,10 @@ package com.example.plush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class staff_SoftwareDetailsScreen extends AppCompatActivity {
 
@@ -11,4 +14,8 @@ public class staff_SoftwareDetailsScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_software_details_screen);
     }
+
+
+
+
 }
