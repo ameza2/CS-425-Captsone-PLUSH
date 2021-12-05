b4776e47cb1aa608ae94495ae972aa4afeaf8652
tszemach17 <tzemach@nevada.unr.edu>
2021-12-04 17:16:43 -0800

    Settings page created
    It is empty for now

---
+++ PLUSHApp/app/src/main/java/com/example/plush/StaffSettingsScreen.java
@@ -0,0 +1,14 @@
+package com.example.plush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffSettingsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_settings_screen);
+    }
+}
