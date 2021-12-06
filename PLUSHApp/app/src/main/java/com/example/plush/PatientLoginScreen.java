1818d8b2e95dbfef3ac10bcc5d2789cadf1bf335
tszemach17 <tzemach@nevada.unr.edu>
2021-12-05 20:30:26 -0800

    Patient login now goes to PLUSH home screen

--- PLUSHApp/app/src/main/java/com/example/plush/PatientLoginScreen.java
+++ PLUSHApp/app/src/main/java/com/example/plush/PatientLoginScreen.java
@@ -2,7 +2,10 @@ package com.example.plush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientLoginScreen extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class PatientLoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login_screen);
+
+        Button button = findViewById(R.id.loginbutton);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(PatientLoginScreen.this, StaffPlushUnitScreen.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
