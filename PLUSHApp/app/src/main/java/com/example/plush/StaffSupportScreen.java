80ed5683ede635e64d31de2fea7da1d8ec8d271a
tszemach17 <tzemach@nevada.unr.edu>
2021-12-06 14:51:15 -0800

    Support Page created

--- PLUSHApp/app/src/main/java/com/example/plush/StaffSupportScreen.java
+++ PLUSHApp/app/src/main/java/com/example/plush/StaffSupportScreen.java
@@ -2,13 +2,27 @@ package com.example.plush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StaffSupportScreen extends AppCompatActivity {
+
+    Button supportButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_support_screen);
+
+        supportButton = findViewById(R.id.submitButton);
+        supportButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(StaffSupportScreen.this, StaffHomeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
