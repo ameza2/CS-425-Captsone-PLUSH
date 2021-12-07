a3907406ea2f031e165afde3d23ec54bd44fe2ee
Abraham Meza <ameza2@nevada.unr.edu>
2021-12-06 19:38:03 -0800

    Code Commenting (2/2)
    Code Comments Done

--- PLUSHApp/app/src/main/java/com/example/plush/StaffSupportScreen.java
+++ PLUSHApp/app/src/main/java/com/example/plush/StaffSupportScreen.java
@@ -1,28 +1,33 @@
+// Utility Package //
 package com.example.plush;
 
+// Libraries //
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+/* Android Widgets */
 import android.widget.Button;
 
-public class StaffSupportScreen extends AppCompatActivity {
+public class StaffSupportScreen extends AppCompatActivity { // StaffSupportScreen w/ action activities
 
-    Button supportButton;
+    Button supportButton; // button variable: supportButton (submit support request and redirect user to home screen)
 
+    /* Initialize Page Activity (Staff Support Screen) */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_support_screen);
 
         supportButton = findViewById(R.id.submitButton);
-        supportButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+
+        /* Support Button: used to submit support request and redirect user to home screen */
+        supportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent intent = new Intent(StaffSupportScreen.this, StaffHomeScreen.class);
-                startActivity(intent);
+                startActivity(intent); // page redirection (StaffHomeScreen)
             }
         });
     }
-
 }
