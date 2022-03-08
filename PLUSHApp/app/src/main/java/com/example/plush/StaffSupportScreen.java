0c56159f66d0fc1cd10f3383282263d62df2c018
Abraham Meza <ameza2@nevada.unr.edu>
2022-03-07 16:54:33 -0800

    Email Validation
    Email Validation using modified OWASP Regular Expression

--- PLUSHApp/app/src/main/java/com/example/plush/StaffSupportScreen.java
+++ PLUSHApp/app/src/main/java/com/example/plush/StaffSupportScreen.java
@@ -10,27 +10,53 @@ import android.view.View;
 /* Android Widgets */
 import android.widget.Button;
 import android.widget.EditText;
-
-import android.util.Log;
 import android.widget.Toast;
 
+/* Android Debugger */
+import android.util.Log;
+
+/* Regular Expression: Input Validation */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StaffSupportScreen extends AppCompatActivity { // StaffSupportScreen w/ action activities
 
-    private static final String regex = "^(.+)@(.+)$";
+    /*
+
+    Modified Version of OWASP Validation Regex Repository
+
+    Local Part Restrictions:
+        - Allows numeric values from 0 to 9
+        - Allows Uppercase and Lowercase characters
+        - Allows Symbols: underscore “_”, hyphen “-”, dot “.”, and plus "+" : no pipe (|) or single quote (') -> prevents SQL Injections
+        - Dot "." not allowed at the start and end
+        - Consecutive Dots ".." not allowed
+
+    Domain Part Restrictions
+        - Allows numeric values from 0 to 9 *
+        - Allows Uppercase and Lowercase characters
+        - Allows Symbols: hyphen "-" and dot "." : no pipe (|) or single quote (') -> prevents SQL Injections
+        - Dot “.” not allowed at the start and end
+        - Hyphen “-” not allowed at end (maybe have this start too)
+        - Consecutive Dots ".." not allowed
+    */
+
+    private static final String regex = "^[a-zA-Z0-9_+-]+(?:\\.[a-zA-Z0-9_+-]+)*@" // local part regular expression
+            + "(?:[^-.]+[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"; // domain part regular expression
 
     Button supportButton; // button variable: supportButton (submit support request and redirect user to home screen)
-    EditText supportName;
-    EditText supportEmail;
-    EditText supportDesc;
+    EditText supportName; // textbox variable: supportName (store user name)
+    EditText supportEmail; // textbox variable: supportEmail (store user email)
+    EditText supportDesc; // textbox variable: supportDesc (store support message contents)
 
     /* Initialize Page Activity (Staff Support Screen) */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_support_screen);
+
+        // Fetch Button and Textbox States //
 
         supportButton = findViewById(R.id.submitButton);
         supportName = findViewById(R.id.textName);
@@ -38,52 +64,54 @@ public class StaffSupportScreen extends AppCompatActivity { // StaffSupportScree
         supportDesc = findViewById(R.id.textDescription);
 
         /* Support Button: used to submit support request and redirect user to home screen */
-        //Referencing solution from https://stackoverflow.com/questions/8994488/android-button-onclick-submit-to-email
-        supportButton.setOnClickListener(new View.OnClickListener() {
+        // Referencing solution from https://stackoverflow.com/questions/8994488/android-button-onclick-submit-to-email //
+        supportButton.setOnClickListener(new View.OnClickListener() { // form submission activity
             public void onClick(View v) {
 
-                boolean emptyName = supportName.getText().toString().isEmpty();
-                boolean emptyEmail = supportEmail.getText().toString().isEmpty();
-                boolean emptyDesc = supportDesc.getText().toString().isEmpty();
+                // Input Validation Checks //
 
-                if ((emptyName && emptyEmail) | (emptyName && emptyDesc) | (emptyEmail && emptyDesc) | (emptyName && emptyEmail && emptyDesc)){
-                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing multiple fields.", Toast.LENGTH_LONG).show(); // deactivation prompt
+                boolean emptyName = supportName.getText().toString().isEmpty(); // boolean: check for empty 'Name' field
+                boolean emptyEmail = supportEmail.getText().toString().isEmpty(); // boolean: check for empty 'Email' field
+                boolean emptyDesc = supportDesc.getText().toString().isEmpty(); // boolean: check for empty 'Message' field
+
+                if ((emptyName && emptyEmail) | (emptyName && emptyDesc) | (emptyEmail && emptyDesc) | (emptyName && emptyEmail && emptyDesc)) { // if statement: if multiple input fields are empty, promopt error message to user
+                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing multiple fields.", Toast.LENGTH_LONG).show(); // error message confirmation
                     //Log.d("Error [1]: ", "Empty Text Field");
                 }
-                else if (emptyName) { // Input Text Validation: Required Fields
-                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing Name.", Toast.LENGTH_LONG).show(); // deactivation prompt
+                else if (emptyName) { // else if statement: else if 'Name' field is empty, prompt error message
+                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing Name.", Toast.LENGTH_LONG).show(); // error message confirmation
                     //Log.d("Error [2]: ", "Empty Text Field");
                 }
-                else if (emptyEmail){
-                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing Email Address.", Toast.LENGTH_LONG).show(); // deactivation prompt
+                else if (emptyEmail) { // else if statement: else if 'Email' field is empty, prompt error message
+                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing Email Address.", Toast.LENGTH_LONG).show(); // error message confirmation
                     //Log.d("Error [3]: ", "Empty Text Field");
                 }
-                else if (emptyDesc) {
-                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing Message Description.", Toast.LENGTH_LONG).show(); // deactivation prompt
+                else if (emptyDesc) { // else if statement: else if 'Message' field is empty, prompt error message
+                    Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing Message Description.", Toast.LENGTH_LONG).show(); // error message confirmation
                     //Log.d("Error [4]: ", "Empty Text Field");
                 }
-                else {
+                else { // else statement: else if input fields passed validation check, proceed with form submission process
 
-                    Pattern pattern = Pattern.compile(regex);
-                    Matcher matcher = pattern.matcher(supportEmail.getText().toString());
+                    // Email Validation Algorithm using Regular Expressions //
 
-                    if (!matcher.matches()) {
-                        Toast.makeText(getApplicationContext(), "Invalid Email Address", Toast.LENGTH_LONG).show();
+                    Pattern pattern = Pattern.compile(regex); // compile regular expression
+                    Matcher matcher = pattern.matcher(supportEmail.getText().toString()); // check regular expression against email input
+
+                    if (!matcher.matches()) { // if statement: if email input does not pass email validation, prompt error message
+                        Toast.makeText(getApplicationContext(), "Invalid Email Address", Toast.LENGTH_LONG).show(); // error message confirmation (invalid email)
                         //Log.d("Error [5]: ", "Invalid Email Address");
                     }
-                    else {
-                        Intent email = new Intent(Intent.ACTION_SEND);
-                        email.putExtra(Intent.EXTRA_EMAIL, new String[]{"PLUSHassistance@gmail.com"});
-                        email.putExtra(Intent.EXTRA_SUBJECT, "PLUSH Support Request");
-                        email.putExtra(Intent.EXTRA_TEXT, supportName.getText().toString() + "\n"
+                    else { // else statement: else if email input passes email validation, submit form using email sharing feature
+                        Intent email = new Intent(Intent.ACTION_SEND); // android share action
+                        email.putExtra(Intent.EXTRA_EMAIL, new String[]{"PLUSHassistance@gmail.com"}); // target email address (PLUSH)
+                        email.putExtra(Intent.EXTRA_SUBJECT, "PLUSH Support Request"); // email subject line (SUPPORT)
+                        email.putExtra(Intent.EXTRA_TEXT, supportName.getText().toString() + "\n" // email content (include user name and email for future contact)
                                 + supportEmail.getText().toString() + "\n\n"
                                 + supportDesc.getText().toString());
 
-                        //need this to prompts email client only
-                        email.setType("message/rfc822");
+                        email.setType("message/rfc822"); // prompts email client only
 
-                        //Should launch the user's email app allowing the user to choose which app to send the email.
-                        startActivity(Intent.createChooser(email, "Choose an Email client :"));
+                        startActivity(Intent.createChooser(email, "Choose an Email client :")); // launch user's email app allowing the user to choose which app to send the email
                     }
                 }
             }
